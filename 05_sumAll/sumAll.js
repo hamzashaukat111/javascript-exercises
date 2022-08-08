0d16bac37fc222566718966c687ddf41a0742a1c
hamzashaukat111 <75229817+hamzashaukat111@users.noreply.github.com>
2022-08-09 03:49:39 +0500

    more js exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  // let num;
+  if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+  if (x < 0 || y < 0) return "ERROR";
+  const bignum = x > y ? x : y;
+  const smallnum = x < y ? x : y;
+  let sum = 0;
 
+  for (let i = smallnum; i <= bignum; i++) {
+    sum = sum + i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
