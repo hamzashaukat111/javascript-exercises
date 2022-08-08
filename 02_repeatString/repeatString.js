07bac2fbf2b0909cea0f436ecc73f03dd67fda83
hamzashaukat111 <75229817+hamzashaukat111@users.noreply.github.com>
2022-08-09 00:13:01 +0500

    Did javaScript different Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+if(times<0) return 'ERROR';
+let hi = '';
+for(let i=0;i<times;i++){
+hi+= word;}
+return hi;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (ok, ...x) {
+  //   const arr = x[0];
+  //   const toRemove = x[1];
+  const newArr = [];
 
+  //   const toRemove = [...x];
+  for (const i of ok) {
+    if (!x.includes(i)) {
+      newArr.push(i);
+    }
+  }
+  return newArr;
 };
 
 // Do not edit below this line
